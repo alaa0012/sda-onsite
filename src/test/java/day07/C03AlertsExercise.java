9c98d8cbb8a182d59c03ca02412318447d513b6e
alaa alshaqha <alaaalshaqha@MacBook-Pro-alkhas-b-alaa.local>
2024-03-03 10:00:31 +0300

    3/3

---
+++ src/test/java/day07/C03AlertsExercise.java
@@ -0,0 +1,24 @@
+package day07;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+//*Go to URL: http://demo.automationtesting.in/Alerts.html
+//Click "Alert with OK" and click 'click the button to display an alert box:'
+//Accept Alert(I am an alert box!) and print alert on console.
+//Click "Alert with OK & Cancel" and click 'click the button to display a confirm box'
+//Cancel Alert  (Press a Button !)
+//Click "Alert with Textbox" and click 'click the button to demonstrate the prompt box'
+//And then sendKeys «Bootcamp» (Please enter your name)
+//Finally print on console this message "Hello Bootcamp How are you today" assertion these message.*//
+
+public class C03AlertsExercise {
+
+
+}
