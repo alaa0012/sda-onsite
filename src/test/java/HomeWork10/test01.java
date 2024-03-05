2c9694dca2e3094ad566d1dc0631f6e878f90a6a
alaa alshaqha <alaalshaqha@gmail.com>
2024-03-05 17:15:32 +0300

    last Upd3/5

---
+++ src/test/java/HomeWork10/test01.java
@@ -0,0 +1,41 @@
+package HomeWork10;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import Utilities.TestBase;
+
+public class test01 extends TestBase {
+
+    // private WebDriver driver;
+
+    @Test
+    public void testYouTubeSearch() {
+        // Go to YouTube
+        driver.get("https://www.youtube.com/");
+
+        // Find the search box and enter "interstellar"
+        WebElement searchBox = driver.findElement(By.name("search_query"));
+        searchBox.sendKeys("interstellar");
+
+        // Press Enter to perform the search
+        searchBox.sendKeys(Keys.ENTER);
+
+        // Click on the first video that appears
+        WebElement firstVideo = driver.findElement(By.cssSelector("#contents ytd-video-renderer"));
+        firstVideo.click();
+
+        // Assertions
+        String videoTitle = driver.findElement(By.cssSelector("#container h1.title yt-formatted-string")).getText();
+        Assert.assertTrue(videoTitle.contains("Interstellar"));
+
+        // You can add more assertions or test steps here
+        driver.findElement(By.xpath("//button[@id='search-icon-legacy']//yt-icon[@class='style-scope ytd-searchbox']//div")).click();
+
+        WebElement firstVideo = driver.findElement(By.cssSelector("a#video-title"));
+        firstVideo.click();
+        // ...
+    }
+
+}
---
+++ src/test/java/day10/C02JavaScriptExecutor02.java
@@ -0,0 +1,43 @@
+package day10;
+
+import Utilities.JScriptUtilities;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import Utilities.TestBase;
+
+
+public class C02JavaScriptExecutor02 extends TestBase {
+    /*
+    Go to URL: https://api.jquery.com/dblclick/
+    Double click on the blue square at the bottom of the page
+    Write the changed color on search box on the top of the page
+    Click "jQuery in Action" at the bottom of the page.
+
+     */
+    @Test
+    public void test(){
+
+        driver.get("https://api.jquery.com/dblclick/");
+        driver.switchTo().frame(0);
+        WebElement colouredButton =driver.findElement(By.cssSelector("body>div"));
+
+        JScriptUtilities.scrollIntoViewJS(driver,colouredButton);
+
+        actions
+                .doubleClick(colouredButton)
+                .perform();
+
+        String colorCode = colouredButton.getCssValue("background-color");
+
+        driver.switchTo().defaultContent();
+
+        JScriptUtilities.scrollAllUpByJS(driver);
+
+        driver.findElement(By.name("s")).sendKeys(colorCode);
+
+        JScriptUtilities.scrollDownByJS(driver);
+        driver.findElement(By.xpath("//a[@href = 'https://www.manning.com/books/jquery-in-action-third-edition']")).click();
+
+    }
+}
